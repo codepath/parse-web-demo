b7e9604dfff0bb6044911851663cc57147575dd2
Timothy Lee <tim@codepath.org>
2022-07-05 15:15:59 -0700

    Load Parse keys from Heroku environment

--- parse-demo-api/config.js
+++ parse-demo-api/config.js
@@ -1,5 +1,5 @@
-const PARSE_APP_ID = ""
-const PARSE_JAVASCRIPT_KEY = ""
+const PARSE_APP_ID = process.env.PARSE_APP_ID
+const PARSE_JAVASCRIPT_KEY = process.env.PARSE_JAVASCRIPT_KEY
 
 module.exports = {
     PARSE_APP_ID,
